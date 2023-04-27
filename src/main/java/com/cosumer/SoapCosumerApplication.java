08a661334f874207d10e5aedd433e0f0ed461a81
Sadat (Mohammad Sadat Hussain Rafsanjani) <BS04@citybank.bd>
2023-04-27 16:18:05 +0600

    first commit

---
+++ src/main/java/com/cosumer/SoapCosumerApplication.java
@@ -0,0 +1,13 @@
+package com.cosumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCosumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCosumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosumer/SoapCosumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCosumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
